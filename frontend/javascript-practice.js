1dadd5eb9e5835e338e8a7ccdbacbd3127fed238
pradeepthi <pradeepthibokka@gmail.com>
2026-06-19 14:31:21 +0530

    added javascript file

--- frontend/javascript-practice.js
+++ frontend/javascript-practice.js
@@ -4,17 +4,33 @@
 // let a="10"
 // console.log(a)
 // }
-let a=10
-let b=20
-let c=90
-if(a>b && a>c)
+
+
+// let a=10
+// let b=20
+// let c=90
+// if(a>b && a>c)
+// {
+//     console.log(a)
+// }
+// else if(b>a && b>c)
+// {
+//     console.log(b)
+// }
+// else{
+//     console.log(c)
+// }
+
+const age = 19
+const license =true
+if(age>=18)
 {
-    console.log(a)
+    if(license)
+    {
+        console.log("can drive")
+    }
 }
-else if(b>a && b>c)
+else
 {
-    console.log(b)
-}
-else{
-    console.log(c)
+    console.log("cannot drive")
 }
